15ebb67b6b8927c7e79a731627ee756b429d2c0e
hung <ssvnn1996@gmail.com>
2020-01-06 19:47:15 +0700

    Create 4 new table

---
+++ 2019_12_23_135235_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('category_id');
+            $table->string('product_name', 255);
+            $table->integer('price');
+            $table->string('image', 255);
+            $table->integer('quantity');
+            $table->tinyint('avg_rating');
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
